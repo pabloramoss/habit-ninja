8bf2dde59c8ffa60bb54e08e29ab603d9c4bdf00
pabloramoss <pablo-362@hotmail.com>
2023-02-20 10:33:20 -0300

    first commit

---
+++ src/pages/graph.tsx
@@ -0,0 +1,13 @@
+const GraphPage: React.FC = () => {
+  return(
+    <div style={{display: 'flex', gap:'2px', width: 500, height: 200, background: "white", justifyContent: "center"}} className="graph">
+      <input type="checkbox" />
+      <input type="checkbox" />
+      <input type="checkbox" />
+      <input type="checkbox" />
+      <input type="checkbox" />
+      </div>
+  )
+}
+
+export default GraphPage
